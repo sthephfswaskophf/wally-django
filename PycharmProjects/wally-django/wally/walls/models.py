f794a3df541e668eac493bdf2dca41042d69a025
sthephfswaskophf <swaskophf@icloud.com>
2021-07-16 22:25:24 -0300

    deploy_2

--- PycharmProjects/wally-django/wally/walls/models.py
+++ PycharmProjects/wally-django/wally/walls/models.py
@@ -2,16 +2,17 @@ from django.db import models
 
 class Devedor (models.Model):
 
-    name = models.TextField()
-    id_debtor = models.BigIntegerField()
-    id_espaider = models.IntegerField()
-    phone_1 = models.IntegerField()
-    phone_2 = models.IntegerField()
-    phone_hot = models.IntegerField()
-    phone_whatsapp = models.IntegerField()
+    id = models.AutoField(primarykey=True)
+    name = models.CharField('Nome/Razão Social' max_length=50, blank=True, null=True)
+    id_tax = models.BigIntegerField('CNPJ/CPF')
+    id_espaider = models.IntegerField('ID Espaider')
+    phone_1 = models.IntegerField('Telefone')
+    phone_2 = models.IntegerField('Telefone')
+    phone_hot = models.IntegerField('Telefone Hot')
+    phone_whatsapp = models.IntegerField('Whatsapp')
 
 
-    id_debtor = models.ForeignKey("Devedor", on_delete=models.SET_NULL, null=True)
+
 
 
 
